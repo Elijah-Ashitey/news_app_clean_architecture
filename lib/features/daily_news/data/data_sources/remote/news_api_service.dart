89fcfa7d05d652069395eface49e40b82878fa37
Elijah Ashitey <39532130+Elijah-Ashitey@users.noreply.github.com>
2024-05-25 00:56:21 +0000

    final

---
+++ lib/features/daily_news/data/data_sources/remote/news_api_service.dart
@@ -0,0 +1,18 @@
+import 'dart:io';
+
+import 'package:news_app_clean_architecture/features/daily_news/data/models/article.dart';
+import 'package:retrofit/http.dart';
+import 'package:dio/dio.dart';
+import '../../../../../core/constants/constants.dart';
+part 'news_api_service.g.dart';
+
+@RestApi(baseUrl: newsAPIBaseURL)
+abstract class NewsApiService {
+  factory NewsApiService(Dio dio) = _NewsApiService;
+  @GET('/top-headlines')
+  Future<HttpResponse<List<ArticleModel>>> getNewsArticles({
+    @Query("apiKey") String? apiKey,
+    @Query("country") String? country,
+    @Query("category") String? category,
+  }) {}
+}
